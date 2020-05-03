c86fd7c274fa498527bde1102b995f5193b85190
Milkenm <miguel.campos.johnny@gmail.com>
2020-05-03 19:05:54 +0100

    Problema 1

---
+++ ConsoleApp/ConsoleApp/Problema1.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <locale.h>
+
+double Pot(double A, int N)
+{
+	return N == 0 ? 1 : (A * Pot(A, N - 1));
+}
+
+
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+
+	double A = 0;
+	int N = 0;
+
+	printf("Insira o número base: "); scanf_s("%lf", &A);
+	printf("Insira a potência: "); scanf_s("%d", &N);
+
+	printf("\nResultado:    %0.2lf ^ %i = %0.2lf\n\n", A, N, Pot(A, N));
+
+	return 0;
+}
