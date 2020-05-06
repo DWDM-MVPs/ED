a398dc226b73027a978ed87a7cbf54b39adadcdf
Milkenm <miguel.campos.johnny@gmail.com>
2020-05-06 03:13:21 +0100

    Problema 2 (funciona mais pra menos que pra mais)

--- ConsoleApp/ConsoleApp/Problema1.cpp
+++ ConsoleApp/ConsoleApp/Problema1.cpp
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 #include <locale.h>
 
@@ -25,3 +26,4 @@ int main()
 
 	return 0;
 }
+*/
---
+++ ConsoleApp/ConsoleApp/Problema2.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+const char* Replace(const char* string, const char* filter, const char* replacement)
+{
+	char buffer[4096];
+	const char* p;
+	int i = 0;
+
+	while (string[i])
+	{
+		if ((p = strstr(string + i, filter)) == false)
+		{
+			return string;
+		}
+
+		strncpy(buffer + strlen(buffer), string + i, (p - string) - i);
+		buffer[p - string] = '\0';
+		strcat(buffer, replacement);
+		printf("STR: %s\n", buffer);
+		i = (p - string) + strlen(filter);
+	}
+
+	return string;
+}
+
+const char* ConvertePastaWin_To_Unix(const char* pasta_win)
+{
+	return Replace(pasta_win, "\\", "//");
+}
+
+const char* ConvertePastaUnix_To_Win(const char* pasta_unix)
+{
+	return Replace(pasta_unix, "//", "\\");
+}
+
+
+
+
+
+int main()
+{
+	const char* pasta_WIN = "C:\\Users\\FMorgado\\Pictures\\Saved Pictures\\Video.mp4";
+	const char* pasta_UNIX = "C://Users//FMorgado//Pictures//Saved Pictures//Video.mp4";
+
+	printf("Pasta WIN > UNIX: %s\n", ConvertePastaWin_To_Unix(pasta_WIN));
+	printf("Pasta UNIX > WIN: %s\n", ConvertePastaUnix_To_Win(pasta_UNIX));
+
+	return 0;
+}
